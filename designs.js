187adef293fd9e620663150a03a27aed5e7a1a18
oliverputtick <puttick.oliver@gmail.com>
2021-05-03 09:55:49 +0100

    new variables added, grid now dynamic

--- designs.js
+++ designs.js
@@ -4,22 +4,26 @@
 const colourPicker = document.querySelector("#colorPicker");
 const pixelCanvas = document.querySelector("#pixelCanvas");
 const submitButton = document.querySelector("#sizePicker input[type=button]")
+//const rows = document.querySelector("#inputHeight").value;
+//const cells = document.querySelector("#inputWidth").value;
 
 // When size is submitted by the user, call makeGrid()
 submitButton.addEventListener("click", function() {
-    // submitButton.preventDefault();
-    makeGrid();
+    let rows = document.querySelector("#inputHeight").value;
+    let cells = document.querySelector("#inputWidth").value;
+    makeGrid(rows, cells);
 });
 
 // Make grid function (using comments for separation)
-function makeGrid() {
+function makeGrid(rows, cells) {
+    pixelCanvas.innerHTML = null
 
     // Loop creates "tr"
-    for (let x = 0; x < 10; x++) {
+    for (let x = 0; x < rows; x++) {
         let row = document.createElement("tr");
 
         // Loop creates "td"
-        for (let y = 0; y < 10; y++) {
+        for (let y = 0; y < cells; y++) {
             let cell = document.createElement("td");
             row.appendChild(cell);
         }

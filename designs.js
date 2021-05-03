ca548f7cfb152f0b3200973dd45a185108c6a886
oliverputtick <puttick.oliver@gmail.com>
2021-05-03 08:45:12 +0100

    Updated sizePicker variable name to submitButton

--- designs.js
+++ designs.js
@@ -1,10 +1,12 @@
 // Select color input
 // Select size input
+// Select sumbit button
 const colourPicker = document.querySelector("#colorPicker");
 const pixelCanvas = document.querySelector("#pixelCanvas");
+const submitButton = document.querySelector("#sizePicker input[type=submit]")
 
 // When size is submitted by the user, call makeGrid()
-document.querySelector("#sizePicker input[type=submit]").addEventListener("click", function() {
+submitButton.addEventListener("click", function() {
     alert("Hello, this is a test");
 });
 

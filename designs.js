b9c78485604484225f0ed6c75a6987417aca4698
oliverputtick <puttick.oliver@gmail.com>
2021-05-03 08:41:08 +0100

    Added JS alert to test event

--- designs.js
+++ designs.js
@@ -4,7 +4,11 @@ const colourPicker = document.querySelector("#colorPicker");
 const pixelCanvas = document.querySelector("#pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+document.querySelector("#sizePicker input[type=submit]").addEventListener("click", function() {
+    alert("Hello, this is a test");
+});
 
+// Make grid function (using comments for separation)
 function makeGrid() {
 
 }; 

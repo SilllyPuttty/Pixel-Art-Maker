5f915e121308fa3944efd2faeb3bf91b7e83c5cb
oliverputtick <puttick.oliver@gmail.com>
2021-05-03 08:12:10 +0100

    Global variables added

--- designs.js
+++ designs.js
@@ -1,10 +1,10 @@
 // Select color input
 // Select size input
+const colourPicker = document.querySelector("#colorPicker");
+const pixelCanvas = document.querySelector("#pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
-// Your code goes here!
-
-}
+}; 
